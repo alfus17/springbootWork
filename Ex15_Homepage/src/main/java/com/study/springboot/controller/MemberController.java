2cacb4b15ab45ceed28f6c0b1d3243fdd54e0a55
JangGwangJin <alfus9802@naver.com>
2024-08-13 19:21:24 +0900

    @gjjang

---
+++ Ex15_Homepage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -0,0 +1,67 @@
+package com.study.springboot.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.study.springboot.domain.Member;
+import com.study.springboot.service.MemberService;
+
+@Controller
+@SessionAttributes({"loginUser"})
+public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
+	
+	@GetMapping("/enrollForm")
+	public String enrollForm() {
+		return "member/enrollForm";
+	}
+	
+	@GetMapping("/idCheck")
+	@ResponseBody
+	public boolean idCheck(@RequestParam("id") String id) {
+		return memberService.idCheck(id);
+	}
+	
+	@PostMapping("/memberInsert")
+	public String memberInsert(Member member) {
+		String enPass = passwordEncoder.encode(member.getPassword());
+		member.setPassword(enPass);
+		memberService.memberInsert(member);
+		return "redirect:/";
+	}
+	
+	@PostMapping("/login")
+	public String login(Member member, Model model) {
+		Optional<Member> loginUser = memberService.login(member);
+		if(loginUser.isPresent()) {
+			Member m = loginUser.get();
+			System.out.println("MemberController_loginUser : "+ m);
+			if(passwordEncoder.matches(member.getPassword(), m.getPassword())) {
+				model.addAttribute("loginUser", m);
+			}
+		}
+		
+		return "redirect:/";
+	}
+
+}
